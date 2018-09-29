0217515d60339b101af5e3f0f2edaebe1b6b129a
Bishal Gautam <bishalkrgautam@gmail.com>
2018-09-29 13:54:54 -0500

    EmployeeControllerCreated

--- EmployeeService_TokenAuthentication/Controllers/EmployeesController.cs
+++ EmployeeService_TokenAuthentication/Controllers/EmployeesController.cs
@@ -4,10 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using EmployeeDAL;
 
 namespace EmployeeService_TokenAuthentication.Controllers
 {
+    [Authorize]
     public class EmployeesController : ApiController
     {
+        public IEnumerable<EmployeesTokenExampleTable> Get()
+        {
+            using(EmployeeDBEntities entities= new EmployeeDBEntities())
+            {
+                return entities.EmployeesTokenExampleTables.ToList();
+            }
+        }
     }
+
 }
+  
