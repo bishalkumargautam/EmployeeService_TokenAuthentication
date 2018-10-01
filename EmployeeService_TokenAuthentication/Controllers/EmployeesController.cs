8f3db857aa0139669b3d9c51bd0569820c50751e
Bishal Gautam <bishalkrgautam@gmail.com>
2018-10-01 11:40:31 -0500

    Token Authentication, WebApi Login, LogOff, Hello {Username} implemented

--- EmployeeService_TokenAuthentication/Controllers/EmployeesController.cs
+++ EmployeeService_TokenAuthentication/Controllers/EmployeesController.cs
@@ -16,7 +16,7 @@ namespace EmployeeService_TokenAuthentication.Controllers
             using(EmployeeDBEntities entities= new EmployeeDBEntities())
             {
                 return entities.EmployeesTokenExampleTables.ToList();
-
+ 
             }
         }
     }
